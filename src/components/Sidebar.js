74e4629a5934fc04ec64b6d0eecfad8480386f0b
ghollamsimo <ghollamsimo1@gmail.com>
2023-02-20 12:48:31 +0100

    changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,7 +24,7 @@ const Sidebar = () => {
     <IoMdArrowForward className='text-2xl' />
     </div>
     </div>
-    <div className='flex flex-col gap-y-2 h-[300px] lg:h-[300px] overflow-y-auto overflow-x-hidden border-b'>
+    <div className='flex flex-col gap-y-2 h-[400px] lg:h-[450px] overflow-y-auto overflow-x-hidden border-b'>
       {cart.map((item) =>{
       return <CartItem item={item} key={item.id} />;
     })}
@@ -38,7 +38,7 @@ const Sidebar = () => {
           <FiTrash/>
           </div>
       </div>
-      <Link to='/' className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium rounded-md'>View Cart</Link>
+      
       <Link to='/' className='bg-primary flex p-4 justify-center items-center text-white w-full font-medium rounded-xl'>
         Checkout
       </Link>
