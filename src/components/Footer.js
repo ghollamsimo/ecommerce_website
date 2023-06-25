9fd593a0035fb714a155c05fdea41cb68a1c07f4
ghollamsimo <111603947+ghollamsimo@users.noreply.github.com>
2023-06-25 18:01:39 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return <footer className='bg-primary p-12'>
     <div className='container mx-auto'>
       <p className='text-white text-center'>
-        Copyright &copy; GHOLLAM 2023. All rights reserved.
+        Copyright &copy; Ghollam 2023. All rights reserved.
       </p>
     </div>
   </footer>;
